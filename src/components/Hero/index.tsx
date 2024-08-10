25449b488b4ea861ddbfd8ae0d77c9d8599f1235
Felpszinho <felpsdev95@gmail.com>
2024-08-10 18:24:37 -0300

    upando os arquivos

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Formulario, HeroTitle } from './style'
+
+const Hero = () => (
+  <Formulario>
+    <Container>
+      <HeroTitle>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTitle>
+    </Container>
+  </Formulario>
+)
+
+export default Hero
